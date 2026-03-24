3779a79fd1010ca34de10f278a3721a54cffc395
Sanyam Raj Singh Dodiya <sanyamrajsingh@gmail.com>
2026-03-24 20:06:20 +0530

    Change DB connection password to empty string
    Updated database connection password to an empty string.

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1@Dodiddone",
+        password=" ",
         database="ehr_system",
         autocommit=True,
         connection_timeout=30
